81c03969ede6e64447ea3bb72e3d9b875f107444
Olajcodes <olajideabioye01@gmail.com>
2025-10-24 05:46:08 +0100

    API endpoint done completed

--- week_11/FASTAPI/app.py
+++ week_11/FASTAPI/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+from typing import Optional
 import uvicorn
 import os
 
@@ -28,16 +29,38 @@ def get_data():
 
 @app.post("/create-data")
 def create_data(req: Item):
-    data.append(req.dict())
+    data.append(req.model_dump())
     print(data)
     return {"Message": "Data Received", "Data": data}
 
 @app.put("/update-data/{id}")
 def update_data(id: int, req: Item):
-    data[id] = req.dict()
+    data[id] = req.model_dump()
     print(data)
     return {"Message": "Data Updated", "Data": data}
+
+# Creating class to validate patching
+class validate_patch(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    track: Optional[str] = None
+
+# Implementing the update(PATCH) function
+@app.patch("/patch-data/{id}")
+def patch_data(id: int, req: validate_patch):
+    if id >= len(data):
+        return {"Error": "Index out of range."}
+    data[id].update(req.model_dump(exclude_unset=True))
+    return {"Message": "Data Edited", "data": data}
     
+    
+# Implementing the delete(REMOVE) function
+@app.delete("/delete-data/{id}")
+def delete_data(id: int):
+    if id >= len(data):
+        return {"Error": "Index out of range."}
+    data.pop(id)
+    return {"Message": "Data Deleted", "data": data}
 
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
