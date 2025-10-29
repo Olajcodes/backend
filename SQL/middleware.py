e6eb3961ca1dfddd6ad55b77614d62b10d603a56
Olajcodes <olajideabioye01@gmail.com>
2025-10-29 16:10:17 +0100

    Pending testing

--- SQL/middleware.py
+++ SQL/middleware.py
@@ -1,44 +1,32 @@
-# import jwt
-# from dotenv import load_dotenv
-# import os
-# from datetime import datetime, timedelta
-
-# load_dotenv()
-
-# secret_key = os.getenv("secret_key")
-
-# def create_token(details:dict, expiry: int):
-#     expire = datetime.now() + timedelta(minutes=expiry)
-    
-#     details.update({"exp":expire})
-    
-#     encoded_jwt = jwt.encode(details, secret_key)
-    
-#     return encoded_jwt
-    
 import jwt
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 from datetime import datetime, timedelta
-from fastapi import Request
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials  #get bearer code and get authorization
-from fastapi import Security
-bearer = HTTPBearer()
+from fastapi import Request, Depends, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import security
+
 load_dotenv()
-secret_key = os.getenv("secret_key")
-def create_token(details: dict, expiry: int):
-    expire = datetime.now() + timedelta(minutes=expiry)
-    details.update({"exp": expire})
-    encoded_jwt = jwt.encode(details, secret_key)
-    return encoded_jwt
-# To get token
-def verify_token(request: HTTPAuthorizationCredentials = Security(bearer)):
-    token = request.credentials
-    # to split to stringg
-    # token = payload.split(" ")[1]   #to split what is coming from space between
-    verified_token = jwt.decode(token, secret_key, algorthm=["HS256"])
-    # expiry_time = verified_token.get("exp")
-    return {
-        "email": verified_token.get("email"),
-        "userType": verified_token.get("userType")
-    }
+
+bearer = HTTPBearer()
+
+secret_key= os.getenv("secret_key")
+
+def create_token(details:dict, expiry:int):
+    expire= datetime.now() + timedelta(minutes=expiry)
+
+    details.update({"exp":expire})
+    # sign jwt
+    encoder_jwt=jwt.encode(details, secret_key)
+    return encoder_jwt
+
+def verify_token(request: HTTPAuthorizationCredentials= Security (bearer)):
+    # payload = request.headers.get("Authorization")
+    token= request.credentials
+    
+    # token = payload.split(" ")[1]
+    verify_token=jwt.decode(token, secret_key, algorithms=["HS256"])
+    
+    # expiry_time= verify_token.get("exp")
+
+    return{"email":verify_token.get("email"), "userType":verify_token.get("userType"), "id": verify_token.get("id")}
--- SQL/simpleapp.py
+++ SQL/simpleapp.py
@@ -1,23 +1,37 @@
 from database import db
-from fastapi import FastAPI, HTTPException # To handle ty exception error
+from fastapi import FastAPI, Depends, HTTPException # To handle ty exception error
 from pydantic import BaseModel, Field
 from sqlalchemy import text
 import os
 from dotenv import load_dotenv
 import bcrypt
 import uvicorn
-from middleware import create_token
+from middleware import create_token, verify_token
+
+
 load_dotenv()
 
 app = FastAPI(title="Simple App", version="1.0.0")
 
-token_time = int(os.getenv("token_time"))
+expiry_time = int(os.getenv("token_time"))
 
 class Simple(BaseModel):
-    name: str = Field(..., examples=["Abdullateef"])
-    email: str = Field(..., examples=["abc@gmail.com"])
-    password: str = Field(..., examples=["abdul123"])
-    userType: str = Field(..., examples=["student"])
+    name: str = Field(..., example= "Abdullateef")
+    email: str = Field(..., example= "abc@gmail.com")
+    password: str = Field(..., example= "abdul123")
+    userType: str = Field(..., example= "student")
+    gender: str = Field(..., example= "Male")
+
+class LoginRequest(BaseModel):
+    email: str = Field(..., example= "sam@gmail.com")
+    password: str = Field(..., example= "sam123")
+    
+class CourseRequest(BaseModel):
+    title: str = Field(..., example="Backend Course")
+    level: str = Field(..., example="Beginner")
+    
+class Enroll(BaseModel):
+    courseId: int = Field(..., example=1)
     
 @app.get("/", description="This endpoint returns a welcome message")       # Adding a decorator 
 def root():
@@ -26,60 +40,55 @@ def root():
 @app.post("/signup")
 def signUp(input: Simple):
     try:
-        duplicate_query = text("""
-            SELECT * FROM users
-            WHERE email = :email
-                               """)
+        duplicate_query = text(""" SELECT * FROM users WHERE email = :email """)
         # Checking for an existing details
-        existing = db.execute(duplicate_query, {"email": input.email})
-        if existing:
+        existing = db.execute(duplicate_query, {"email": input.email}).fetchone()
+        
+        if existing is not None:
             print("Email already exists")
             raise HTTPException(status_code=400, detail="Email already exists")
         
         query = text("""
-              INSERT INTO users (name, email, password)
-              VALUES(:name, :email, :password)       # We used ":" as a placeholder here.
+              INSERT INTO users (name, email, password, userType, gender)
+              VALUES(:name, :email, :password, :userType, :gender)       # We used ":" as a placeholder here.
         """)
-        # To encrypt our password, we need to install bcrypt
+        # To encrypt our password (i.e, Hashing), we need to install bcrypt
         salt = bcrypt.gensalt()     # Salt is needed here to automatically generate random values to the enconded password created to differentiate from each other
         hashedPassword = bcrypt.hashpw(input.password.encode('utf-8'), salt)
-        print(hashedPassword)
+        print(hashedPassword)       # printing this to the terminal
         
-        db.execute(query, {"name": input.name, "email": input.email, "password": hashedPassword, "userType": input.userType})
+        # Mapping data
+        data = {"name": input.name, "email": input.email, "password": hashedPassword, "userType": input.userType, "gender": input.gender}
+        db.execute(query, data)
         db.commit()     # Did this to commit changes after inputting.
         
         return{
             "message": "User created successfully",
-            "data": {"name": input.name, "email": input.email, "userType": input.userType}
+            "data": {"name": input.name, "email": input.email, "userType": input.userType, "gender": input.gender}
         }
     except Exception as e:
-        raise HTTPException(status_code=500, detail=0)
-    
-# TO build a login endpoint, let's create a class for it
-class LoginRequest(BaseModel):
-    email: str = Field(..., examples=["sam@gmail.com"])
-    password: str = Field(..., examples=["sam123"])
+        raise HTTPException(status_code=500, detail=str(e))
     
 @app.post("/login")
 def login(input: LoginRequest):
     try:
-        query = text("""
-            SELECT * from users WHERE email = :email
-                     """)
+        query = text(""" 
+         SELECT * from users WHERE email = :email 
+        """)
         result = db.execute(query, {"email": input.email}).fetchone()        # using `fetchone` here, because only one instance is needed for comparing
-
         if not result:
-            
-            raise HTTPException(status_code=401, detail= "Invalid email or Password")
+            raise HTTPException(status_code=404, detail= "Invalid email or Password")
+        
         verified_password = bcrypt.checkpw(input.password.encode('utf-8'), result.password.encode('utf-8'))
         
         if not verified_password:
             raise HTTPException(status_code=404, detail="Invalid email or password")
         
-        encoded_token = create_token(details={
+        encoded_token = create_token({
+            "id": result.id,
             "email": result.email,
             "userType": result.userType
-        }, expiry=token_time)
+        }, expiry_time)
         
         return {
             "Message": "Login Successful",
@@ -88,32 +97,53 @@ def login(input: LoginRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail= str(e))
     
-    
-# Creating a class for courses
-class CourseRequest(BaseModel):
-    title: str = Field(..., example="Backend Course")
-    level: str = Field(..., example="Beginner")
 
 @app.post("/courses")
 def addcourses(input: CourseRequest, user_data = Depends(verify_token)):
     try:
         print(user_data)
-        
         if user_data.userType != 'admin':
-            raise HTTPException(status_code=401, detail="You are not authorized to")
-        query = text("""
-                INSERT INTO courses (title, level)
-                VALUES(:title, :level)
+            raise HTTPException(status_code=401, detail="You are not authorized to add a course")
+        query = text(""" 
+            INSERT INTO courses (title, level) 
+            VALUES(:title, :level)
         """)
         db.execute(query, {"title": input.title, "level": input.level})
         db.commit()
-        
         return {
             "Message": "Course added successfully",
             "data": {"title": input.title, "level": input.level}
         }
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e) )
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.post("/enroll")
+def enroll_course(input: Enroll, details = Depends(verify_token)):
+    try:
+        query = text("""
+            SELECT * FROM users
+            WHERE id = :id
+        """)
+        existing = db.execute(query, {"id": details["id"]}).fetchone()
+        if existing.userType != "student":
+            raise HTTPException(status_code=400, detail="You are not authorized to enroll!")
+        enroll_query = text("""
+            INSERT INTO enrollments(userId, courseId)
+            VALUES (:userId, :courseId)              
+        """)
+        db.execute(enroll_query, {"userId": details["id"], "courseId": input.courseId})
+        db.commit()
+        
+        return {
+            "message": "User enrolled successfully!",
+            "data": {
+                "UserId": details["id"],
+                "courseId": input.courseId
+            }
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
     
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
